b14607a27ba71d8bb4ce215bd0ab48e7060f5c40
DarkZYT <aouabsss@gmail.com>
2019-04-10 14:01:59 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,8 @@ client.music = require('discord.js-musicbot-addon');
 client.music.start(client, {
   youtubeKey: 'AIzaSyAYP5t2p-qC_vO7lfr06bZr5zzRwDo4a_k' ,
   botPrefix: 'music ',
-  musicPresence: true, 
+  musicPresence: true,
+  nextPresence: { game: { name: 'no song.' , streaming: true}},
   channelWhitelist: ['565273792704806932'],
   bigPicture: true
 });
